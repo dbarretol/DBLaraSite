e063381db460e3b4192cd549afb31cc4c736fb96
dbarretol <57958754+dbarretol@users.noreply.github.com>
2024-09-08 22:49:39 -0500

    Header and footer completed

---
+++ src/common/MainFooter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MainFooter() {
+    return (
+        <footer>
+            <div className="container-fluid" id="Footer">
+                <p>2024 - Todos los derechos reservados</p>
+            </div>
+        </footer>
+    )
+}
+
+export default MainFooter
---
+++ src/common/MainHeader.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Common.css'
+
+function MainHeader() {
+  return (
+    <header>
+    <div className="container-fluid" id="Header">
+      <h1>Dr. Ing. David <strong>Barreto Lara</strong></h1>
+      <p className="fst-italic">
+        🔬Investigador RENACYT | CIP 330188 |👨‍💼Consultor independiente |⚡Energías Renovables |⚙️Gestión e ingeniería de mantenimiento |🧑‍🏫Docente Posgrado | Especialista en Patentes | MBA (c)
+      </p>
+    </div>
+  </header>
+  )
+}
+
+export default MainHeader
