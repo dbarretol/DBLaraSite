9f1af61121437aa3ac25e188289dfe3061ae3271
dbarretol <57958754+dbarretol@users.noreply.github.com>
2024-09-10 21:30:14 -0500

    Problema de enrutamiento solucionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,33 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { BASE_ROUTE } from './config.js';
-// import './App.css'
-import MainHeader from './common/MainHeader'
-import MainFooter from './common/MainFooter'
-import MainNav from './common/MainNav'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import MainHeader from './common/MainHeader';
+import MainFooter from './common/MainFooter';
+import MainNav from './common/MainNav';
 
-import Home from './sections/home/Home'
-import Education from './sections/education/Education.jsx'
+import Home from './sections/home/Home';
+import Education from './sections/education/Education.jsx';
 import Experience from './sections/experience/Experience.jsx';
 import Project from './sections/projects/Projects.jsx';
 import Publications from './sections/publications/Publications.jsx';
 
-
-
 function App() {
-
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <MainHeader />
         <MainNav />
         <main>
           <Routes>
-            <Route path={BASE_ROUTE} element={<Home />} />
-            <Route path={`${BASE_ROUTE}/Estudios`} element={<Education />} />
-            <Route path={`${BASE_ROUTE}/Experiencia`} element={<Experience />} />
-            <Route path={`${BASE_ROUTE}/Proyectos`} element={<Project />} />
-            <Route path={`${BASE_ROUTE}/Publicaciones`} element={<Publications />} />
-
+            <Route path="/" element={<Home />} />
+            <Route path="/Estudios" element={<Education />} />
+            <Route path="/Experiencia" element={<Experience />} />
+            <Route path="/Proyectos" element={<Project />} />
+            <Route path="/Publicaciones" element={<Publications />} />
           </Routes>
-
         </main>
-
-
         <MainFooter />
-      </BrowserRouter>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/common/MainNav.jsx
+++ src/common/MainNav.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import logoUNI from '/assets/icons/logo_UNI.png'
-import { BASE_ROUTE } from '../config';
 
 function MainNav() {
     return (
         <nav className="navbar navbar-expand-lg sticky-top custom-navbar">
             <div className="container-fluid">
-                <Link className="navbar-brand" to={BASE_ROUTE}>
+                <Link className="navbar-brand" to="/">
                     <img src={logoUNI} alt="Logo UNI" height="45" className="d-inline-block align-text-center p-0" />
                 </Link>
                 <button className="navbar-toggler"
@@ -24,41 +23,40 @@ function MainNav() {
                     <ul className="navbar-nav me-auto">
                         {/* Elemento 01 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to={`${BASE_ROUTE}/Estudios`}>Estudios</Link>
+                            <Link className="nav-link" to="/Estudios">Estudios</Link>
                         </li>
                         {/* Elemento 02 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to={`${BASE_ROUTE}/Experiencia`}>Experiencia</Link>
+                            <Link className="nav-link" to="/Experiencia">Experiencia</Link>
                         </li>
                         {/* Elemento 03 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to={`${BASE_ROUTE}/Proyectos`}>Proyectos</Link>
+                            <Link className="nav-link" to="/Proyectos">Proyectos</Link>
                         </li>
                         {/* Elemento 04 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to={`${BASE_ROUTE}/Publicaciones`}>Publicaciones</Link>
+                            <Link className="nav-link" to="/Publicaciones">Publicaciones</Link>
                         </li>
                         {/* Elemento 05 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to={`${BASE_ROUTE}/Certifications`}>Certificaciones</Link>
+                            <Link className="nav-link" to="/Certifications">Certificaciones</Link>
                         </li>
                         {/* Elemento 06 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to={`${BASE_ROUTE}/Awards`}>Premios y reconocimientos</Link>
+                            <Link className="nav-link" to="/Awards">Premios y reconocimientos</Link>
                         </li>
                         {/* Elemento 07 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to={`${BASE_ROUTE}/Other`}>Otras actividades</Link>
+                            <Link className="nav-link" to="/Other">Otras actividades</Link>
                         </li>
                         {/* Elemento 08 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to={`${BASE_ROUTE}/DigitalSkills`}>Competencias digitales</Link>
+                            <Link className="nav-link" to="/DigitalSkills">Competencias digitales</Link>
                         </li>
                         {/* Elemento 09 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to={`${BASE_ROUTE}/Languages`}>Idiomas</Link>
+                            <Link className="nav-link" to="/Languages">Idiomas</Link>
                         </li>
-
                     </ul>
                 </div>
             </div>
