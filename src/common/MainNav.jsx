394cb9a189d889ce3f83e7ece82f65996f9ec7dc
dbarretol <57958754+dbarretol@users.noreply.github.com>
2024-09-11 23:39:32 -0500

    navbar colapso automatico

--- src/common/MainNav.jsx
+++ src/common/MainNav.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import logoUNI from '/assets/icons/logo_UNI.png'
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import logoUNI from '/assets/icons/logo_UNI.png';
 
 function MainNav() {
+    // Hook de estado para controlar el colapso del menú
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+    // Función para manejar el clic en el botón del navbar
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+    // Función para cerrar el menú al hacer clic en un enlace
+    const closeNav = () => setIsNavCollapsed(true);
+
     return (
         <nav className="navbar navbar-expand-lg sticky-top custom-navbar">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">
+                <Link className="navbar-brand" to="/" onClick={closeNav}>
                     <img src={logoUNI} alt="Logo UNI" height="45" className="d-inline-block align-text-center p-0" />
                 </Link>
                 <button className="navbar-toggler"
@@ -14,54 +23,56 @@ function MainNav() {
                     data-bs-toggle="collapse"
                     data-bs-target="#navbarNav"
                     aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
+                    aria-expanded={!isNavCollapsed}
+                    aria-label="Toggle navigation"
+                    onClick={handleNavCollapse}
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                {/* Menu superior*/}
-                <div className="collapse navbar-collapse" id="navbarNav">
+                {/* Menu superior */}
+                <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarNav">
                     <ul className="navbar-nav me-auto">
                         {/* Elemento 01 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to="/Estudios">Estudios</Link>
+                            <Link className="nav-link" to="/Estudios" onClick={closeNav}>Estudios</Link>
                         </li>
                         {/* Elemento 02 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to="/Experiencia">Experiencia</Link>
+                            <Link className="nav-link" to="/Experiencia" onClick={closeNav}>Experiencia</Link>
                         </li>
                         {/* Elemento 03 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to="/Proyectos">Proyectos</Link>
+                            <Link className="nav-link" to="/Proyectos" onClick={closeNav}>Proyectos</Link>
                         </li>
                         {/* Elemento 04 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to="/Publicaciones">Publicaciones</Link>
+                            <Link className="nav-link" to="/Publicaciones" onClick={closeNav}>Publicaciones</Link>
                         </li>
                         {/* Elemento 05 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to="/Certifications">Certificaciones</Link>
+                            <Link className="nav-link" to="/Certifications" onClick={closeNav}>Certificaciones</Link>
                         </li>
                         {/* Elemento 06 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to="/Awards">Premios y reconocimientos</Link>
+                            <Link className="nav-link" to="/Awards" onClick={closeNav}>Premios y reconocimientos</Link>
                         </li>
                         {/* Elemento 07 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to="/Other">Otras actividades</Link>
+                            <Link className="nav-link" to="/Other" onClick={closeNav}>Otras actividades</Link>
                         </li>
                         {/* Elemento 08 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to="/DigitalSkills">Competencias digitales</Link>
+                            <Link className="nav-link" to="/DigitalSkills" onClick={closeNav}>Competencias digitales</Link>
                         </li>
                         {/* Elemento 09 */}
                         <li className="nav-item">
-                            <Link className="nav-link" to="/Languages">Idiomas</Link>
+                            <Link className="nav-link" to="/Languages" onClick={closeNav}>Idiomas</Link>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
-    )
+    );
 }
 
-export default MainNav
+export default MainNav;
